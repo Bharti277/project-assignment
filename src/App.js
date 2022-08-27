b9b05d87c3ec6c4edf3570f08bb1772fb3115081
Bharti277 <bhartideependra277@gmail.com>
2022-08-27 14:45:47 +0530

    reunion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Estatery from './Estatery/Estatery';
+import Search from "./Estatery/Search"
+
+function App() {
+  return (
+    <div className="App">
+     <Estatery />
+    </div>
+  );
+}
+
+export default App;
